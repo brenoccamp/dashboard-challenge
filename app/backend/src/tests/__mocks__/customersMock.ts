30f23aee8c128e483bd21b5814e8ca41a51c70a6
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-05-23 20:23:11 -0400

    :bug: Fix(I&T): Fixed customers mock.

--- app/backend/src/tests/__mocks__/customersMock.ts
+++ app/backend/src/tests/__mocks__/customersMock.ts
@@ -2,13 +2,11 @@ const mockedCustomers = [
   {
     id: 1,
     fullname: 'Fake Customer 1',
-    city: 'Campinas',
     state: 'SP',
   },
   {
     id: 2,
     fullname: 'Fake Customer 2',
-    city: 'Cuiabá',
     state: 'MT',
   },
 ]
