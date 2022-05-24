822f40961dec09d94f42b1f5e74b22e19d851765
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-05-24 18:03:04 -0400

    sparkles: Feat(Context): Created Application Context.

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import ApplicationProvider from './context/provider/ApplicationProvider';
 
 function App() {
+  console.log('APP');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ApplicationProvider> */}
+      <p>TESTE</p>
+      {/* </ApplicationProvider> */}
     </div>
   );
 }
