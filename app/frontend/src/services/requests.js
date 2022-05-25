9b82c774dd1a2e4d4dcd4bd51164b66ec8a88a89
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-05-25 14:26:34 -0400

    :heavy_plus_sign: Build(Seller lightweight): Installed Marquee package.

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -6,7 +6,10 @@ const getAllSales = async () => axios.get(`${API_BASE_URL}/dashboard-data/sales`
 
 const getAllStores = async () => axios.get(`${API_BASE_URL}/dashboard-data/stores`);
 
+const getAllSellers = async () => axios.get(`${API_BASE_URL}/dashboard-data/sellers`);
+
 export {
   getAllSales,
   getAllStores,
+  getAllSellers,
 };
