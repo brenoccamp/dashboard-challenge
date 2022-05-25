ff7be192ca6bee034621e2835c4e85d70345fdfb
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-05-25 09:28:41 -0400

    :lipstick: Feat(Header): Styling header ... Body: Styling header and created a state to manager year filter.

--- app/frontend/src/context/provider/ApplicationProvider.js
+++ app/frontend/src/context/provider/ApplicationProvider.js
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import ApplicationContext from '../ApplicationContext';
 
 function ApplicationProvider({ children }) {
-  const contextValue = {};
+  const [year, setYear] = useState('2020');
+
+  const contextValue = {
+    year,
+    setYear,
+  };
 
   return (
     <ApplicationContext.Provider value={ contextValue }>
