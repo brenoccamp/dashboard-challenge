54cf80e27d33b2e4941c83f2228a0c6b855ba3d8
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-05-25 15:21:06 -0400

    :sparkles: Feat(Sellers compoennt): Display sellers ... Body: Implemented feat to display all sellers in screen when site is loaded.

--- app/frontend/src/components/index.js
+++ app/frontend/src/components/index.js
@@ -1,9 +1,11 @@
 import YearIncome from './YearIncome';
 import Fitlers from './Filters';
 import Header from './Header';
+import Sellers from './Sellers';
 
 export {
   YearIncome,
   Fitlers,
   Header,
+  Sellers,
 };
